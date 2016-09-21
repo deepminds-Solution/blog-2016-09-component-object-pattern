99600313d8f1697967eff4028590e1e167fb30fc
Lubos Krnac <lubos.krnac@gmail.com>
2016-09-21 21:14:11 +0200

    Take project from Page Object example repo
    https://github.com/lkrnac/blog-2016-06-page-object-pattern

---
+++ src/test/java/net/lkrnac/blog/pageobject/e2e/TodoAppTest.java
@@ -0,0 +1,113 @@
+package net.lkrnac.blog.pageobject.e2e;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import net.lkrnac.blog.pageobject.TodoApplication;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by: Lubos Krnac
+ * Date: 2016-07-14.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TodoApplication.class)
+@WebIntegrationTest
+public class TodoAppTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCreateTodos() {
+        // GIVEN
+        new TodoPageObject(driver).get()
+
+            // WHEN
+            .addTodo("testTodo1")
+            .addTodo("testTodo2")
+
+            // THEN
+            .verifyTodoShown("testTodo1", false)
+            .verifyTodoShown("testTodo2", false);
+    }
+
+    @Test
+    public void testCompleteTodo() {
+        // GIVEN
+        new TodoPageObject(driver).get()
+            .addTodo("testTodo1")
+            .addTodo("testTodo2")
+
+            // WHEN
+            .clickOnTodoItem("testTodo1")
+
+            // THEN
+            .verifyTodoShown("testTodo1", true)
+            .verifyTodoShown("testTodo2", false);
+    }
+
+    @Test
+    public void testSelectActive() {
+        // GIVEN
+        new TodoPageObject(driver).get()
+            .addTodo("testTodo1")
+            .addTodo("testTodo2")
+            .clickOnTodoItem("testTodo1")
+
+            // WHEN
+            .selectActive()
+
+            // THEN
+            .verifyTodoNotShown("testTodo1")
+            .verifyTodoShown("testTodo2", false);
+    }
+
+    @Test
+    public void testSelectCompleted() {
+        // GIVEN
+        new TodoPageObject(driver).get()
+            .addTodo("testTodo1")
+            .addTodo("testTodo2")
+            .clickOnTodoItem("testTodo1")
+
+            // WHEN
+            .selectCompleted()
+
+            // THEN
+            .verifyTodoShown("testTodo1", true)
+            .verifyTodoNotShown("testTodo2");
+    }
+
+    @Test
+    public void testSelectAll() {
+        // GIVEN
+        new TodoPageObject(driver).get()
+            .addTodo("testTodo1")
+            .addTodo("testTodo2")
+            .clickOnTodoItem("testTodo1")
+            .selectCompleted()
+
+            // WHEN
+            .selectAll()
+
+            // THEN
+            .verifyTodoShown("testTodo1", true)
+            .verifyTodoShown("testTodo2", false);
+    }
+}
---
+++ src/test/java/net/lkrnac/blog/pageobject/e2e/TodoPageObject.java
@@ -0,0 +1,86 @@
+package net.lkrnac.blog.pageobject.e2e;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static java.lang.String.format;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by: Lubos Krnac
+ * Date: 2016-07-14.
+ */
+public class TodoPageObject {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public TodoPageObject(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+
+    public TodoPageObject get() {
+        driver.get("localhost:8080");
+        wait.until(ExpectedConditions.elementToBeClickable(By.tagName("button")));
+        return this;
+    }
+
+    private WebElement findElementWithText(String text) {
+        return driver.findElement(getConditionForText(text));
+    }
+
+    private List<WebElement> findElementsWithText(String text) {
+        return driver.findElements(getConditionForText(text));
+    }
+
+    private By getConditionForText(String text) {
+        return By.xpath(format("//*[text()='%s']", text));
+    }
+
+    public TodoPageObject addTodo(String todo) {
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys(todo);
+        WebElement button = driver.findElement(By.tagName("button"));
+        button.click();
+        return this;
+    }
+
+    public TodoPageObject clickOnTodoItem(String todoItem) {
+        findElementWithText(todoItem).click();
+        return this;
+    }
+
+    public TodoPageObject selectAll() {
+        findElementWithText("All").click();
+        return this;
+    }
+
+    public TodoPageObject selectActive() {
+        findElementWithText("Active").click();
+        return this;
+    }
+
+    public TodoPageObject selectCompleted() {
+        findElementWithText("Completed").click();
+        return this;
+    }
+
+    public TodoPageObject verifyTodoShown(String todoItem, boolean expectedStrikethrough) {
+        WebElement todoElement = findElementWithText(todoItem);
+        assertNotNull(todoElement);
+        boolean actualStrikethrough = todoElement.getAttribute("style").contains("text-decoration: line-through;");
+        assertEquals(expectedStrikethrough, actualStrikethrough);
+        return this;
+    }
+
+    public TodoPageObject verifyTodoNotShown(String todoItem) {
+        assertTrue(findElementsWithText(todoItem).isEmpty());
+        return this;
+    }
+}
